99472148fb33c64c862b649cb651930ec0ba6862
ryoww <ryotaroshimozawa.0121@gmail.com>
2025-06-14 17:58:38 +0900

    test header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 import { Box, Flex } from "@mantine/core";
 import type React from "react";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 const Header: React.FC = () => {
     return (
         <>
-            <Flex w={"100%"} h={150} gap={0} p={0}>
-                <Box w={"25%"} bg={"blue"}></Box>
-                {/* <Box w={"75%"} bg={"red"}></Box> */}
-                <h1>Header</h1>
+            <Flex w={"100%"} h={"150px"} gap={0} p={0}>
+                <Box w={"25%"} h={"100%"} bg={"blue"}></Box>
+                <Box w={"75%"} h={"100%"} bg={"red"}></Box>
+                {/* <h1>Header</h1> */}
             </Flex>
 
             <Outlet />

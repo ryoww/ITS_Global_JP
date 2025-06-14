88bbce13dd0ce2ff9055cef7ac531f815769a0be
ryoww <ryotaroshimozawa.0121@gmail.com>
2025-06-14 17:39:28 +0900

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
+import { Stack } from "@mantine/core";
+
 function Home() {
-    return <></>;
+    return (
+        <>
+            <Stack>
+                <h1>Home Page</h1>
+                <p>Welcome to the home page of our application!</p>
+            </Stack>
+        </>
+    );
 }
 
 export default Home;

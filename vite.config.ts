6f8593350ed82d8785a0d22808859d2f9d13b60b
ryoww <ryotaroshimozawa.0121@gmail.com>
2025-06-14 20:14:10 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/ITS_Global_JP/",
     build: {
         outDir: "docs",
     },
